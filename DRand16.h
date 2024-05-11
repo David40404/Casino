ef5fdc302927386a48fe238d9e4e50e65636ffe4
David40404 <88509902+David40404@users.noreply.github.com>
2024-05-11 23:11:15 +0300

    Update DRand16.h

--- DRand16.h
+++ DRand16.h
@@ -2,11 +2,12 @@
 #include <time.h>
 
 unsigned short DRand16() {
-    static unsigned int seed = time(NULL);
-    static unsigned int special_seed = seed;
+    static unsigned int seed = (unsigned int)time(NULL);
 
-    special_seed ^= (1 ^ seed) * 3 + 44;
-    seed ^= 47 * (seed >> 2) + 84 + special_seed;
+    static const unsigned int a = 2932355;
+    static const unsigned int b = 5456263;
+
+    seed ^= a * (seed >> 2) + b * seed;
 
     return static_cast <unsigned short> (seed % UINT16_MAX);
 }
